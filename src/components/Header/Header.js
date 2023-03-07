eb2e7d546eea116e54cf42d8bd2e36b2875c17cc
sowmik-sec <sowmik.sec@gmail.com>
2023-03-07 15:40:52 +0600

    active route added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import { faBars, faUser, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useContext, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Logo from "../../assets/images/logo.png";
 import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
 
@@ -17,14 +17,14 @@ const Header = () => {
   return (
     <div className="flex justify-between items-center mx-12 my-2">
       <div className="flex items-center">
-        <Link to="/">
+        <NavLink to="/">
           <img className="w-14 h-12 mr-3" src={Logo} alt="" />
-        </Link>
-        <Link to="/">
+        </NavLink>
+        <NavLink to="/">
           <p className="text-lg font-bold hover:bg-orange-500 cursor-pointer p-2 rounded">
             Ultimate Survival
           </p>
-        </Link>
+        </NavLink>
       </div>
       <div className="md:flex">
         <div onClick={() => setOpen(!open)} className="md:hidden">
@@ -39,24 +39,24 @@ const Header = () => {
             open ? "top-20 md:bg-none my-3 bg-blue-200" : "top-[-500px]"
           }`}
         >
-          <Link
+          <NavLink
             to="/courses"
             className="text-lg font-bold hover:bg-orange-500 cursor-pointer p-2 rounded"
           >
             <p>Courses</p>
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to="/blog"
             className="text-lg font-bold hover:bg-orange-500 cursor-pointer p-2 rounded"
           >
             <p>Blog</p>
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to="/FAQ"
             className="text-lg font-bold hover:bg-orange-500 cursor-pointer p-2 rounded"
           >
             <p>FAQ</p>
-          </Link>
+          </NavLink>
           {user ? (
             <Link
               onClick={handleLogOut}
@@ -66,29 +66,29 @@ const Header = () => {
             </Link>
           ) : (
             <>
-              <Link
+              <NavLink
                 to="/login"
                 className="text-lg font-bold hover:bg-orange-500 cursor-pointer p-2 rounded"
               >
                 Log In
-              </Link>
-              <Link
+              </NavLink>
+              <NavLink
                 to="/signup"
                 className="text-lg font-bold hover:bg-orange-500 cursor-pointer p-2 rounded"
               >
                 Sign Up
-              </Link>
+              </NavLink>
             </>
           )}
           {user?.photoURL ? (
-            <Link to="/user-profile">
+            <NavLink to="/user-profile">
               <img
                 title={user.displayName}
                 className="w-11 rounded-full"
                 src={user.photoURL}
                 alt=""
               />
-            </Link>
+            </NavLink>
           ) : (
             <FontAwesomeIcon icon={faUser} />
           )}
